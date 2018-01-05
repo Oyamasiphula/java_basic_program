c2d7ee97dbc957cdb2210af58e350e56287852a0
Oyamasiphula <oyamasiphula@gmail.com>
2018-01-05 10:20:56 -0800

    added a function with one method for oop use case

---
+++ Rodentia.java
@@ -0,0 +1,18 @@
+public class Rodentia {
+
+public static void main(String[] args) {
+
+}
+public void loopingInJava (String friendsName){
+        for(int i = 0; i < 10; i++) {
+                System.out.println(i +". " + "Hello ," + friendsName);
+        }
+}
+
+public void order() {
+
+        System.out.println("This animal belongs to the Rodentia order.");
+
+}
+
+}
