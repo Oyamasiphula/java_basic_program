60439c63516d996b699a9d9a8e207f0073eb70f2
Oyamasiphula <oyamasiphula@gmail.com>
2018-01-05 10:20:57 -0800

    added a function that displays all three Java primitive data types

---
+++ dataypes.java
@@ -0,0 +1,13 @@
+//basic primtive java data type
+public class basicDataTypes {
+  public static void (String[]args){
+    int year = 2018;
+    char myFirstNamesFirstChar = 'O';
+    Boolean isHot = flase;
+
+  system.out.println(int);
+  system.out.println(char);
+  system.out.println(Boolean);
+  }
+
+}
